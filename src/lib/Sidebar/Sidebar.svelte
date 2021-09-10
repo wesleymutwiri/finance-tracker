45a9731449c086cf434f3c01c3ed51895b2d9ec9
wesleymutwiri <wesleymochiemo@gmail.com>
2021-09-10 16:34:19 +0300

    created sidebar component and added svgs

---
+++ src/lib/Sidebar/Sidebar.svelte
@@ -0,0 +1,152 @@
+<div class="sidebar-container">
+	<div class="logo">
+		<h1>Wesley</h1>
+	</div>
+	<div class="links">
+		<a href="#" class="iconed-link">
+			<svg
+				viewBox="0 0 24 24"
+				width="24"
+				height="24"
+				stroke="currentColor"
+				stroke-width="2"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="css-i6dzq1"
+				><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /><polyline
+					points="9 22 9 12 15 12 15 22"
+				/></svg
+			>
+			Home
+		</a>
+		<a href="#" class="iconed-link">
+			<svg
+				height="24"
+				viewBox="0 0 512 512"
+				width="24"
+				stroke="currentColor"
+				stroke-width="2"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				><g
+					><path
+						d="m200 0h-160c-22.056 0-40 17.944-40 40v224c0 22.056 17.944 40 40 40h160c22.056 0 40-17.944 40-40v-224c0-22.056-17.944-40-40-40zm24 264c0 13.233-10.766 24-24 24h-160c-13.234 0-24-10.767-24-24v-224c0-13.233 10.766-24 24-24h160c13.234 0 24 10.767 24 24z"
+					/><path
+						d="m200 336h-160c-22.056 0-40 17.944-40 40v96c0 22.056 17.944 40 40 40h160c22.056 0 40-17.944 40-40v-96c0-22.056-17.944-40-40-40zm24 136c0 13.233-10.766 24-24 24h-160c-13.234 0-24-10.767-24-24v-96c0-13.233 10.766-24 24-24h160c13.234 0 24 10.767 24 24z"
+					/><path
+						d="m512 472v-224c0-22.056-17.944-40-40-40h-160c-22.056 0-40 17.944-40 40v224c0 22.056 17.944 40 40 40h160c22.056 0 40-17.944 40-40zm-16 0c0 13.233-10.766 24-24 24h-160c-13.234 0-24-10.767-24-24v-224c0-13.233 10.766-24 24-24h160c13.234 0 24 10.767 24 24z"
+					/><path
+						d="m512 136v-96c0-22.056-17.944-40-40-40h-160c-22.056 0-40 17.944-40 40v96c0 22.056 17.944 40 40 40h160c22.056 0 40-17.944 40-40zm-16 0c0 13.233-10.766 24-24 24h-160c-13.234 0-24-10.767-24-24v-96c0-13.233 10.766-24 24-24h160c13.234 0 24 10.767 24 24z"
+					/></g
+				></svg
+			>
+			Dashboard
+		</a>
+		<a href="#" class="iconed-link">
+			<svg
+				viewBox="0 0 24 24"
+				width="24"
+				height="24"
+				stroke="currentColor"
+				stroke-width="2"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="css-i6dzq1"
+				><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" /><polyline
+					points="14 2 14 8 20 8"
+				/><line x1="16" y1="13" x2="8" y2="13" /><line x1="16" y1="17" x2="8" y2="17" /><polyline
+					points="10 9 9 9 8 9"
+				/></svg
+			>
+			Documents
+		</a>
+		<a href="#" class="iconed-link">
+			<svg
+				viewBox="0 0 24 24"
+				width="24"
+				height="24"
+				stroke="currentColor"
+				stroke-width="2"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="bordered"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12" /></svg
+			>
+			Statistics
+		</a>
+		<a href="#" class="iconed-link">
+			<svg
+				height="24"
+				viewBox="0 0 512 512"
+				width="24"
+				stroke="currentColor"
+				stroke-width="2"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				><path
+					d="m256 0c-141.159 0-256 114.841-256 256s114.841 256 256 256 256-114.841 256-256-114.841-256-256-256zm0 482c-124.617 0-226-101.383-226-226s101.383-226 226-226 226 101.383 226 226-101.383 226-226 226z"
+				/><path
+					d="m256 86.751c-93.324 0-169.249 75.925-169.249 169.249s75.925 169.249 169.249 169.249 169.249-75.925 169.249-169.249-75.925-169.249-169.249-169.249zm0 308.498c-76.782 0-139.249-62.467-139.249-139.249s62.467-139.249 139.249-139.249 139.249 62.467 139.249 139.249-62.467 139.249-139.249 139.249z"
+				/><path
+					d="m256 174.428c-44.98 0-81.572 36.593-81.572 81.572s36.592 81.572 81.572 81.572 81.572-36.593 81.572-81.572-36.593-81.572-81.572-81.572zm0 133.144c-28.438 0-51.572-23.135-51.572-51.572s23.135-51.572 51.572-51.572 51.572 23.135 51.572 51.572-23.134 51.572-51.572 51.572z"
+				/></svg
+			>
+			Goals
+		</a>
+		<a href="#" class="iconed-link">
+			<svg
+				viewBox="0 0 24 24"
+				width="24"
+				height="24"
+				stroke="currentColor"
+				stroke-width="2"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="css-i6dzq1"
+				><circle cx="12" cy="12" r="10" />
+				<line x1="12" y1="16" x2="12" y2="12" /><line
+					x1="12"
+					y1="8"
+					x2="12.01"
+					y2="8"
+				/></svg
+			>
+			Information
+		</a>
+	</div>
+</div>
+
+<style>
+	.sidebar-container {
+		display: flex;
+		flex-direction: column;
+		position: fixed;
+		padding: 2rem;
+		background: #ffffff;
+		height: 100vh;
+		width: 300px;
+		align-items: center;
+	}
+
+	.links {
+		display: flex;
+		flex-direction: column;
+	}
+	.iconed-link {
+		display: flex;
+		align-items: center;
+		margin: 1rem 0;
+	}
+	.iconed-link svg {
+		margin-right: 0.5rem;
+	}
+	.bordered {
+		border: 1px solid currentColor;
+		border-radius: 4px;
+	}
+</style>
