2f74a5bf750af097ce667c269930c941d5acfe05
wesleymutwiri <wesleymochiemo@gmail.com>
2021-09-14 03:24:36 +0300

    added light and dark mode toggler

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,52 @@
 <script lang="ts">
 	// import Header from '$lib/header/Header.svelte';
-	import { onMount } from 'svelte';
-	import { theme } from '../stores/theme.store';
-	import { writable } from 'svelte/store';
-
-	// const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-	// const DARK_PREFERENCE = '(prefers-color-scheme: dark)';
-	// const theme = writable(prefersDarkMode.matches ? 'dark' : 'light');
 	import Sidebar from '$lib/Sidebar/Sidebar.svelte';
 	import '../app.css';
+	import { onMount } from 'svelte';
+	
+	const STORAGE_KEY = 'theme';
+
+	const prefersDarkThemes = () => window.matchMedia('(prefers-color-scheme: dark)');
+	const DARK_PREFERENCE = '(prefers-color-scheme: dark)';
+	const THEMES = {
+		DARK: 'dark',
+		LIGHT: 'light'
+	};
+
+	const applyTheme = () => {
+		const preferredTheme = prefersDarkThemes() ? THEMES.DARK : THEMES.LIGHT;
+		let currentTheme = localStorage.getItem(STORAGE_KEY) ?? preferredTheme;
+		// currentTheme = localStorage.getItem(STORAGE_KEY) ??
+		if (currentTheme === THEMES.DARK) {
+			document.body.classList.remove(THEMES.LIGHT);
+			document.body.classList.add(THEMES.DARK);
+		} else {
+			document.body.classList.remove(THEMES.DARK);
+			document.body.classList.add(THEMES.LIGHT);
+		}
+	};
+
+	const toggleTheme = () => {
+		const stored = localStorage.getItem(STORAGE_KEY);
+
+		if (stored) {
+			localStorage.removeItem(STORAGE_KEY);
+		} else {
+			localStorage.setItem(STORAGE_KEY, prefersDarkThemes() ? THEMES.LIGHT : THEMES.DARK);
+		}
+
+		applyTheme();
+	};
+	onMount(() => {
+		applyTheme();
+		window.matchMedia(DARK_PREFERENCE).addEventListener('change', applyTheme);
+	})
+	// const theme = writable(prefersDarkMode.matches ? 'dark' : 'light');
 </script>
 
 <!-- <Header /> -->
 <div class="container">
-	<Sidebar />
+	<Sidebar on:click={toggleTheme}/>
 	<main>
 		<slot />
 	</main>
