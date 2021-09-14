4ea25e6c82f41f041c76dcfe6900f7e22d310bd5
wesleymutwiri <wesleymochiemo@gmail.com>
2021-09-14 22:47:53 +0300

    added transactions component with conditions

---
+++ src/lib/Transactions/TransactionItem.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let title, price, date;
+</script>
+
+<div class="transaction-container">
+	<div class="transaction-title">
+		<h2>{title}</h2>
+	</div>
+	<div class="transaction-amount">
+		<p class:positive={price > 0}>
+			{price} KSH
+		</p>
+	</div>
+	<div class="transaction-date">
+		{date}
+	</div>
+</div>
+
+<style>
+	.transaction-container {
+		display: grid;
+		grid-template-columns: 2fr 1fr 1fr;
+		align-items: center;
+		width: 800px;
+		gap: 1rem;
+	}
+	p {
+		color: #fc8800;
+	}
+
+	.positive {
+		color: #00b0e8;
+	}
+</style>
