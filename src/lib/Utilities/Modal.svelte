93a80b1839ff3346c75c38f55322a0f2d4b3e8c4
wesleymutwiri <wesleymochiemo@gmail.com>
2021-10-07 17:25:11 +0300

    creating goals and tasks app to the tracker

---
+++ src/lib/Utilities/Modal.svelte
@@ -0,0 +1,30 @@
+<div id="background"></div>
+<div class="modal-container">
+	<div class="modal-header">
+		<slot name="modal-header" />
+        some modal
+	</div>
+	<div class="modal-body">
+		<slot name="modal-body" />
+        some modal
+	</div>
+</div>
+
+<style>
+    #background {
+        position: fixed;
+        z-index: 2;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+    .modal-container {
+        position: fixed;
+        z-index: 3;
+        top: 50%;
+        left: 50%;
+        transform: translate(50%, -50%);
+        background: red;
+    }
+</style>
---
+++ src/routes/goals.svelte
@@ -0,0 +1,7 @@
+<script> 
+import Modal from "$lib/Utilities/Modal.svelte";
+</script>
+
+<h1>My goals</h1>
+<button>Add Goal</button>
+<Modal />
