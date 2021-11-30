e3e94a70eb709dea2346d221502045d38e6dd5c4
wesleymutwiri <wesleymochiemo@gmail.com>
2021-12-01 00:05:51 +0300

    added fetch method to API

--- src/routes/transactions.svelte
+++ src/routes/transactions.svelte
@@ -1,6 +1,35 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const url = 'http://localhost:5000/transactions';
+		const res = await fetch(url, {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		if (res.ok) {
+			return {
+				props: {
+					sections: await res.json()
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
 <script>
 	import Table from '$lib/Utilities/Table.svelte';
+	export let sections = [];
 </script>
 
 <h1>Transactions</h1>
-<Table />
+{#if sections}
+	<Table tableData={sections} />
+{/if}
