f2e3e9ffd790efc45037fa47700f17d5b33f57dc
wesleymutwiri <wesleymochiemo@gmail.com>
2022-01-10 13:28:40 +0300

    added dayjs and calendar component

--- src/routes/calendar.svelte
+++ src/routes/calendar.svelte
@@ -1,4 +1,8 @@
+<script>
+	import CalendarComponent from '$lib/Calendar/CalendarComponent.svelte';
+</script>
+
 <h1>A large calendar of personal events</h1>
 <div class="calendar">
-    some calendar here
+	<CalendarComponent />
 </div>
